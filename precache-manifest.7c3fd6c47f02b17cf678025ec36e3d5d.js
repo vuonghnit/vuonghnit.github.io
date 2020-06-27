de8b2727a587ff03f702a88b78339132a7470265
Vuong Ba <bavuongvng@gmail.com>
2020-06-27 09:39:07 +0700

    build

---
+++ precache-manifest.7c3fd6c47f02b17cf678025ec36e3d5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863bbf894bcd62e4a1b2306051a0bdde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14a938dbd0de8a37d96f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a113434906abb1206f3b",
+    "url": "/static/js/2.f817c454.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.f817c454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a938dbd0de8a37d96f",
+    "url": "/static/js/main.ccd32131.chunk.js"
+  },
+  {
+    "revision": "fae39dd47ad9a00bb02c",
+    "url": "/static/js/runtime-main.c24f648c.js"
+  },
+  {
+    "revision": "6ee1888b087fbeb70f6d1b25df0b0d6c",
+    "url": "/static/media/avatar.6ee1888b.png"
+  }
+]);
